76dff154c343e06d4281cd204c0922018120adfd
sik-regrets <60068106+syncx-bots@users.noreply.github.com>
2021-07-29 16:27:44 +0800

    Create ex4.py

---
+++ Easy/ex4.py
@@ -0,0 +1,6 @@
+# Write a program which accepts a sequence of comma-separated numbers from console and generate a list and a tuple which contains every number. Suppose the following input is supplied to the program: 34,67,55,33,12,98 Then, the output should be: ['34', '67', '55', '33', '12', '98'] ('34', '67', '55', '33', '12', '98')
+
+
+str = input("Enter the comma separated values: ")
+str = str.split(",")
+print("{} {}".format(str,tuple(str)))
