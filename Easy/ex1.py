9ef364d05ec8d8896f118e53b4a230e4863f9671
sik-regrets <60068106+syncx-bots@users.noreply.github.com>
2021-07-29 16:07:07 +0800

    Create ex1.py

---
+++ Easy/ex1.py
@@ -0,0 +1,12 @@
+# Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (bot included). The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+start = 2000
+end = 3200
+x = []
+for i in range(int(start),int(end+1)):
+    if i % 7 == 0 and i % 5 != 0:
+        x.append(str(i))
+print(",".join(x))
+
+
+
