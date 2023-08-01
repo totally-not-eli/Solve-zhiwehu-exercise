69d68e40aaa257253bfd7405ce006f9968988b18
Eliniel Valdez <60068106+totally-not-eli@users.noreply.github.com>
2023-08-01 09:31:43 +0800

    use list comprehension for faster runtime

--- Easy/ex1.py
+++ Easy/ex1.py
@@ -2,8 +2,5 @@
 
 start = 2000
 end = 3200
-x = []
-for i in range(int(start),int(end+1)):
-    if i % 7 == 0 and i % 5 != 0:
-        x.append(str(i))
+x = [i for i in range(start,end) if (i % 7 == 0 and i % 5 != 0)] 
 print(",".join(x))
